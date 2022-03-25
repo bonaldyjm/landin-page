923629686dd6ba92d747c1179e09062b09ecb8e5
jose <bonaldyjm@gmail.com>
2022-03-25 19:12:28 -0300

    pagina reparada

--- src/header.js
+++ src/header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './header.css';
-import RUS2 from './assets/img/RUS2.png'
 
 function Header() {
     return (
         <div id='container-header'>
-        <img src={RUS2} className="header-logo"></img>
+        <img src='./assets/img/RUS.png' className="header-logo"/>
         
         <h1 className='header-titulo'> Inicio </h1>
         </div>
